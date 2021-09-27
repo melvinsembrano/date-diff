bea1709fb01e5a8f19f506a03b696d0213c681ce
Melvin Sembrano <melv@hey.com>
2021-09-27 23:53:32 +1000

    add publish workflow

--- dist/date-diff.d.ts
+++
@@ -1,8 +0,0 @@
-export default class DateDiff {
-    date1: Date;
-    date2: Date;
-    constructor(date1: Date, date2: Date);
-}
-export default DateDiff;
-
-//# sourceMappingURL=date-diff.d.ts.map
--- dist/legacy/date-diff.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=date-diff.js.map
